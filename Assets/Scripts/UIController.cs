f372d8dd9f3a5ef8919db44af765ce8d21ba22f8
Lorena M <lorena-m@web.de>
2015-09-12 10:52:01 +0200

    Added assets of StartScene

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+
+	private static bool groupIsVisible;
+
+
+
+	public void showHideGroup(CanvasGroup group ){
+		if (!groupIsVisible) {
+				group.alpha = 1;
+				group.interactable = true;
+				group.blocksRaycasts = true;
+				groupIsVisible = true;
+		} else {
+				group.alpha = 0;
+				group.interactable = false;
+				group.blocksRaycasts = false;
+				groupIsVisible = false;
+		}
+	}
+
+	public void showImage(Image playImage){
+		playImage.enabled = true;
+	}
+
+	public void changeScene(string scene){
+		Application.LoadLevel (scene);
+	}
+
+
+}
